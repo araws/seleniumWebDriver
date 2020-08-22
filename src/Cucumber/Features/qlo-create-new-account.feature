28148162d2d76e3ba695eaa6c46b777266aa5e25
Andrzej Rawski <rawski.andrzej@gmail.com>
2020-08-22 12:22:29 +0200

    Initial commit

---
+++ src/Cucumber/Features/qlo-create-new-account.feature
@@ -0,0 +1,4 @@
+Feature: Account creation
+  Scenario: Create an account
+
+    Given User is no the https://qloapps.coderslab.pl/pl/logowanie?back=my-account page
---
+++ src/test/java/bdd/QloAccountCreate.java
@@ -0,0 +1,9 @@
+package bdd;
+
+import io.cucumber.java.en.Given;
+
+public class QloAccountCreate {
+    @Given("User is no the https:\\/\\/qloapps.coderslab.pl\\/pl\\/logowanie?back=my-account page")
+    public void userIsNoTheHttpsQloappsCoderslabPlPlLogowanieBackMyAccountPage() {
+    }
+}
