e6341b561b782a2415f849375954b8488dc83420
Andrzej Rawski <rawski.andrzej@gmail.com>
2020-08-24 19:37:58 +0200

    refactor with PageFactory usage

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,34 +1,44 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
     private WebDriver driver;
 
+    @FindBy(name = "email")
+    private WebElement loginInput;
+
+    @FindBy(name = "password")
+    private WebElement passwordInput;
+
+    @FindBy(id = "submit-login")
+    private WebElement singInButton;
+
+    @FindBy(xpath = "//a[@class='account']")
+    private WebElement userName;
+
     public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
         this.driver = driver;
     }
 
     public void loginAs(String email, String password) {
-        WebElement loginInput = driver.findElement(By.name("email"));
         loginInput.click();
         loginInput.clear();
         loginInput.sendKeys(email);
 
-        WebElement passwordInput = driver.findElement(By.name("password"));
         passwordInput.click();
         passwordInput.clear();
         passwordInput.sendKeys(password);
 
-        WebElement singInButton = driver.findElement(By.id("submit-login"));
         singInButton.click();
     }
 
     public String getLoggedUserName(){
-        WebElement userName = driver.findElement(By.xpath("//a[@class='account']"));
         return userName.getText();
     }
 }
