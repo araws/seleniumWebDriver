7d30f66bed908a4b67853c0f09e67fdf43cff6cf
Andrzej Rawski <rawski.andrzej@gmail.com>
2020-09-03 19:19:48 +0200

    BDD applied. Test methods created

--- src/test/java/mystore/MyStoreAddAddress.java
+++ src/test/java/mystore/MyStoreAddAddress.java
@@ -1,4 +1,109 @@
 package mystore;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class MyStoreAddAddress {
+    @Given("^User is created in to CodersLab shop, one address is created and User is logged out$")
+    public void userIsCreatedInToCodersLabShopOneAddressIsCreatedAndUserIsLoggedOut() {
+    }
+
+    @When("^User goes to Login Page$")
+    public void userGoesToLoginPage() {
+    }
+
+    @When("^User fills Email \"([^\"]*)\"$")
+    public void userFillsEmail(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^User fills Password \"([^\"]*)\"$")
+    public void userFillsPassword(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^User clicks Sing in button$")
+    public void userClicksSingInButton() {
+    }
+
+    @Then("^User is logged in to CodersLab shop and is on Your account site$")
+    public void userIsLoggedInToCodersLabShopAndIsOnYourAccountSite() {
+    }
+
+    @When("^User clicks Addresses button$")
+    public void userClicksAddressesButton() {
+    }
+
+    @Then("^User goes to Your addresses page$")
+    public void userGoesToYourAddressesPage() {
+    }
+
+    @And("^There is one address made$")
+    public void thereIsOneAddressMade() {
+    }
+
+    @When("^User clicks Create new address button$")
+    public void userClicksCreateNewAddressButton() {
+    }
+
+    @And("^User fills Alias \"([^\"]*)\" in form$")
+    public void userFillsAliasInForm(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^User fills Address \"([^\"]*)\" in form$")
+    public void userFillsAddressInForm(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^User fills City \"([^\"]*)\" in form$")
+    public void userFillsCityInForm(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^User fills Zip \"([^\"]*)\" in form$")
+    public void userFillsZipInForm(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^User chooses Country from list$")
+    public void userChoosesCountryFromList() {
+    }
+
+    @And("^User fills Phone \"([^\"]*)\" in form$")
+    public void userFillsPhoneInForm(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^User clicks Save button in form$")
+    public void userClicksSaveButtonInForm() {
+    }
+
+    @Then("^User sees \"([^\"]*)\" on page$")
+    public void userSeesOnPage(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^Data is correct in new address$")
+    public void dataIsCorrectInNewAddress() {
+    }
+
+    @When("^User clicks Delete button in new address$")
+    public void userClicksDeleteButtonInNewAddress() {
+    }
+
+    @And("^There is only one previous made address on page$")
+    public void thereIsOnlyOnePreviousMadeAddressOnPage() {
+    }
 }
